c30d324d80253fa7203e0f04fa2449084c173cd2
IvelisBecker <ivelisbecker@gmail.com>
2025-03-19 08:34:18 -0500

    Fixed problems with main.jsx and App.jsx. You SHOULD NOT change anything from there. However, we cannot see navbar or Welcome.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,26 @@
-export default function Contact() {
+import React from "react";
+import { ReactComponent as ReactIcon } from '../assets/react.svg';
+
+const SocialMediaLink = () => {
     return (
-        <div>
-            <h1>Contact Me!</h1>
-            <ul>
-                <li>LinkedIn: </li>
-                <li>Email: </li>
-                <li>GitHub: </li>
-                <li>Click here to download Resume</li>
-            </ul>
-        </div>
-    );
+        <a href="https://www.linkedin.com/in/ivelis-becker/" target="_blank" rel="noopener noreferrer">
+            <ReactIcon url="https://linkedin.com/in/couetilc" style={{ width: '50px', height: '50px' }} />
+        </a>
+    )
 }
+
+export default SocialMediaLink;
+
+// export default function Contact() {
+//     return (
+//         <div>
+//             <h1>Contact Me!</h1>
+//             <ul>
+//                 <li><a>LinkedIn: </a></li>
+//                 <li><a>Email: </a></li>
+//                 <li><a>GitHub: </a></li>
+//                 <li><a>Click here to download Resume</a></li>
+//             </ul>
+//         </div>
+//     );
+// }
