833a6bfabfcdbca4ff6218e073212b0b7d2c217c
IvelisBecker <ivelisbecker@gmail.com>
2025-03-19 20:09:29 -0500

    All fixed. Add info and ready to go!

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function NavTabs() {
+export default function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
@@ -48,4 +48,3 @@ function NavTabs() {
   );
 }
 
-export default NavTabs;
