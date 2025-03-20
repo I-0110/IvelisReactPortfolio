fa8a6a7907d004d95ea33b7d493bb34659bbc8f1
IvelisBecker <ivelisbecker@gmail.com>
2025-03-20 11:47:21 -0500

    Set up navbar horizontal and made portfolio name linked

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to My Portolio!</h1>
+            <h2>Showcasing my work and projects</h2>
+        </div>
+    );
+}
